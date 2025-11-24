2d014e0e97a43435eb583def07fdc715b0112e88
ChoiHyoSeok <klasun1072@gmail.com>
2025-11-24 20:46:40 +0900

    Json3 clear

---
+++ ProjectCode/lec-06-prg-03-json-example/lec-06-prg-03-json-example.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string fileName = "lec-06-prg-03-json-example.json";
+
+        string jsonString = File.ReadAllText(fileName);
+
+        JsonDocument JsonDoc = JsonDocument.Parse(jsonString);
+
+        Console.WriteLine(JsonDoc.RootElement.GetProperty("homeTown"));
+        Console.WriteLine(JsonDoc.RootElement.GetProperty("active"));
+        Console.WriteLine(JsonDoc.RootElement.GetProperty("members")[1].GetProperty("powers")[2]);
+    }
+}
