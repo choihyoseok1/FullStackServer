68663c3db1beed9ca08280ad65acafd0e21091ed
ChoiHyoSeok <klasun1072@gmail.com>
2025-11-27 22:40:50 +0900

    6번 해결

---
+++ ProjectCode/lec-06-prg-06-json-example/lec-06-prg-06-json-example.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string jsonString_Source = @"{
+        ""squadName"": ""Super hero squad"",
+        ""homeTown"": ""Metro City"",
+        ""formed"": 2016,
+        ""secretBase"": ""Super tower"",
+        ""active"": true,
+        ""members"": [
+        {
+        ""name"": ""Molecule Man"",
+        ""age"": 29,
+        ""secretIdentity"": ""Dan Jukes"",
+        ""powers"": [
+        ""Radiation resistance"",
+        ""Turning tiny"",
+        ""Radiation blast""
+        ]
+        },
+        {
+        ""name"": ""Madame Uppercut"",
+        ""age"": 39,
+        ""secretIdentity"": ""Jane Wilson"",
+        ""powers"": [
+        ""Million tonne punch"",
+        ""Damage resistance"",
+        ""Superhuman reflexes""
+        ]
+        },
+        {
+        ""name"": ""Eternal Flame"",
+        ""age"": 1000000,
+        ""secretIdentity"": ""Unknown"",
+        ""powers"": [
+        ""Immortality"",
+        ""Heat Immunity"",
+        ""Inferno"",
+        ""Teleportation"",
+        ""Interdimensional travel""
+        ]
+        }
+        ]
+        }";
+
+        JsonDocument JsonDoc = JsonDocument.Parse(jsonString_Source);
+        var JsonOptions = new JsonSerializerOptions{WriteIndented = true};
+        string jsonString_mid = JsonSerializer.Serialize(JsonDoc.RootElement, JsonOptions);
+        var jsonString_end = JsonSerializer.Deserialize<JsonDocument>(jsonString_mid);
+        Console.WriteLine(jsonString_end.RootElement.GetProperty("homeTown"));
+    }
+}
